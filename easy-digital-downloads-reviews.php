032a95d75c4cf313b353c5e4215d518e828e9e4b
Mithun Biswas <bhoot.biswas@gmail.com>
2020-05-27 22:46:18 +0600

    Add initial files

---
+++ easy-digital-downloads-reviews.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Easy Digital Downloads Reviews
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     easy-digital-downloads-reviews
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Easy_Digital_Downloads_Reviews
+ */
+
+// Your code starts here.
