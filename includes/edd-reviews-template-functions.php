8dd662a70fcb7c8f5adfb5762371a5f68b6e1f68
Mithun Biswas <bhoot.biswas@gmail.com>
2020-06-04 23:46:07 +0600

    Add edd_reviews_get_star_rating_html()

--- includes/edd-reviews-template-functions.php
+++ includes/edd-reviews-template-functions.php
@@ -76,8 +76,32 @@ function edd_reviews_get_rating_html( $rating, $count = 0 ) {
 	if ( 0 < $rating ) {
 		/* translators: %s: rating */
 		$label = sprintf( __( 'Rated %s out of 5', 'edd-reviews' ), $rating );
-		$html  = '<div class="star-rating" role="img" aria-label="' . esc_attr( $label ) . '">' . wc_get_star_rating_html( $rating, $count ) . '</div>';
+		$html  = '<div class="star-rating" role="img" aria-label="' . esc_attr( $label ) . '">' . edd_reviews_get_star_rating_html( $rating, $count ) . '</div>';
 	}
 
 	return apply_filters( 'edd_reviews_download_get_rating_html', $html, $rating, $count );
+}
+
+/**
+ * Get HTML for star rating.
+ *
+ * @since  0.1.0
+ * @param  float $rating Rating being shown.
+ * @param  int   $count  Total number of ratings.
+ * @return string
+ */
+function edd_reviews_get_star_rating_html( $rating, $count = 0 ) {
+	$html = '<span style="width:' . ( ( $rating / 5 ) * 100 ) . '%">';
+
+	if ( 0 < $count ) {
+		/* translators: 1: rating 2: rating count */
+		$html .= sprintf( _n( 'Rated %1$s out of 5 based on %2$s customer rating', 'Rated %1$s out of 5 based on %2$s customer ratings', $count, 'edd-reviews' ), '<strong class="rating">' . esc_html( $rating ) . '</strong>', '<span class="rating">' . esc_html( $count ) . '</span>' );
+	} else {
+		/* translators: %s: rating */
+		$html .= sprintf( esc_html__( 'Rated %s out of 5', 'edd-reviews' ), '<strong class="rating">' . esc_html( $rating ) . '</strong>' );
+	}
+
+	$html .= '</span>';
+
+	return apply_filters( 'edd_reviews_get_star_rating_html', $html, $rating, $count );
 }
