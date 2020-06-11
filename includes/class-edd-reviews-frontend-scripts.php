0fe9c37f84bd7bbbe1632017cbfcc7134be9e759
Mithun Biswas <bhoot.biswas@gmail.com>
2020-06-11 13:38:51 +0600

    Add EDD_Reviews_Frontend_Scripts class

---
+++ includes/class-edd-reviews-frontend-scripts.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * Handle frontend scripts
+ *
+ * @package EDD_Reviews/Classes
+ * @version 0.1.0
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Frontend scripts class.
+ */
+class EDD_Reviews_Frontend_Scripts {
+
+	/**
+	 * Contains an array of script handles registered by WC.
+	 *
+	 * @var array
+	 */
+	private static $scripts = array();
+
+	/**
+	 * Contains an array of script handles registered by WC.
+	 *
+	 * @var array
+	 */
+	private static $styles = array();
+
+	/**
+	 * Contains an array of script handles localized by WC.
+	 *
+	 * @var array
+	 */
+	private static $wp_localize_scripts = array();
+
+	/**
+	 * Hook in methods.
+	 */
+	public static function init() {
+		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
+	}
+
+	/**
+	 * Get styles for the frontend.
+	 *
+	 * @return array
+	 */
+	public static function get_styles() {
+		$version = EDD_REVIEWS_VERSION;
+
+		return apply_filters(
+			'edd_reviews_enqueue_styles',
+			array(
+				'edd-reviews-general' => array(
+					'src'     => self::get_asset_url( 'build/index.css' ),
+					'deps'    => '',
+					'version' => $version,
+					'media'   => 'all',
+					'has_rtl' => true,
+				),
+			)
+		);
+	}
+
+	/**
+	 * Return asset URL.
+	 *
+	 * @param string $path Assets path.
+	 * @return string
+	 */
+	private static function get_asset_url( $path ) {
+		return apply_filters( 'edd_reviews_get_asset_url', plugins_url( $path, EDD_REVIEWS_PLUGIN_FILE ), $path );
+	}
+
+	/**
+	 * Register a script for use.
+	 *
+	 * @uses   wp_register_script()
+	 * @param  string   $handle    Name of the script. Should be unique.
+	 * @param  string   $path      Full URL of the script, or path of the script relative to the WordPress root directory.
+	 * @param  string[] $deps      An array of registered script handles this script depends on.
+	 * @param  string   $version   String specifying script version number, if it has one, which is added to the URL as a query string for cache busting purposes. If version is set to false, a version number is automatically added equal to current installed WordPress version. If set to null, no version is added.
+	 * @param  boolean  $in_footer Whether to enqueue the script before </body> instead of in the <head>. Default 'false'.
+	 */
+	private static function register_script( $handle, $path, $deps = array( 'jquery' ), $version = EDD_REVIEWS_VERSION, $in_footer = true ) {
+		self::$scripts[] = $handle;
+		wp_register_script( $handle, $path, $deps, $version, $in_footer );
+	}
+
+	/**
+	 * Register and enqueue a script for use.
+	 *
+	 * @uses   wp_enqueue_script()
+	 * @param  string   $handle    Name of the script. Should be unique.
+	 * @param  string   $path      Full URL of the script, or path of the script relative to the WordPress root directory.
+	 * @param  string[] $deps      An array of registered script handles this script depends on.
+	 * @param  string   $version   String specifying script version number, if it has one, which is added to the URL as a query string for cache busting purposes. If version is set to false, a version number is automatically added equal to current installed WordPress version. If set to null, no version is added.
+	 * @param  boolean  $in_footer Whether to enqueue the script before </body> instead of in the <head>. Default 'false'.
+	 */
+	private static function enqueue_script( $handle, $path = '', $deps = array( 'jquery' ), $version = EDD_REVIEWS_VERSION, $in_footer = true ) {
+		if ( ! in_array( $handle, self::$scripts, true ) && $path ) {
+			self::register_script( $handle, $path, $deps, $version, $in_footer );
+		}
+		wp_enqueue_script( $handle );
+	}
+
+	/**
+	 * Register a style for use.
+	 *
+	 * @uses   wp_register_style()
+	 * @param  string   $handle  Name of the stylesheet. Should be unique.
+	 * @param  string   $path    Full URL of the stylesheet, or path of the stylesheet relative to the WordPress root directory.
+	 * @param  string[] $deps    An array of registered stylesheet handles this stylesheet depends on.
+	 * @param  string   $version String specifying stylesheet version number, if it has one, which is added to the URL as a query string for cache busting purposes. If version is set to false, a version number is automatically added equal to current installed WordPress version. If set to null, no version is added.
+	 * @param  string   $media   The media for which this stylesheet has been defined. Accepts media types like 'all', 'print' and 'screen', or media queries like '(orientation: portrait)' and '(max-width: 640px)'.
+	 * @param  boolean  $has_rtl If has RTL version to load too.
+	 */
+	private static function register_style( $handle, $path, $deps = array(), $version = EDD_REVIEWS_VERSION, $media = 'all', $has_rtl = false ) {
+		self::$styles[] = $handle;
+		wp_register_style( $handle, $path, $deps, $version, $media );
+
+		if ( $has_rtl ) {
+			wp_style_add_data( $handle, 'rtl', 'replace' );
+		}
+	}
+
+	/**
+	 * Register and enqueue a styles for use.
+	 *
+	 * @uses   wp_enqueue_style()
+	 * @param  string   $handle  Name of the stylesheet. Should be unique.
+	 * @param  string   $path    Full URL of the stylesheet, or path of the stylesheet relative to the WordPress root directory.
+	 * @param  string[] $deps    An array of registered stylesheet handles this stylesheet depends on.
+	 * @param  string   $version String specifying stylesheet version number, if it has one, which is added to the URL as a query string for cache busting purposes. If version is set to false, a version number is automatically added equal to current installed WordPress version. If set to null, no version is added.
+	 * @param  string   $media   The media for which this stylesheet has been defined. Accepts media types like 'all', 'print' and 'screen', or media queries like '(orientation: portrait)' and '(max-width: 640px)'.
+	 * @param  boolean  $has_rtl If has RTL version to load too.
+	 */
+	private static function enqueue_style( $handle, $path = '', $deps = array(), $version = EDD_REVIEWS_VERSION, $media = 'all', $has_rtl = false ) {
+		if ( ! in_array( $handle, self::$styles, true ) && $path ) {
+			self::register_style( $handle, $path, $deps, $version, $media, $has_rtl );
+		}
+		wp_enqueue_style( $handle );
+	}
+
+	/**
+	 * Register/queue frontend scripts.
+	 */
+	public static function enqueue_scripts() {
+		// CSS Styles.
+		$enqueue_styles = self::get_styles();
+		if ( $enqueue_styles ) {
+			foreach ( $enqueue_styles as $handle => $args ) {
+				if ( ! isset( $args['has_rtl'] ) ) {
+					$args['has_rtl'] = false;
+				}
+
+				self::enqueue_style( $handle, $args['src'], $args['deps'], $args['version'], $args['media'], $args['has_rtl'] );
+			}
+		}
+	}
+
+	/**
+	 * Localize a WC script once.
+	 *
+	 * @since 2.3.0 this needs less wp_script_is() calls due to https://core.trac.wordpress.org/ticket/28404 being added in WP 4.0.
+	 * @param string $handle Script handle the data will be attached to.
+	 */
+	private static function localize_script( $handle ) {
+		if ( ! in_array( $handle, self::$wp_localize_scripts, true ) && wp_script_is( $handle ) ) {
+			$data = self::get_script_data( $handle );
+
+			if ( ! $data ) {
+				return;
+			}
+
+			$name                        = str_replace( '-', '_', $handle ) . '_params';
+			self::$wp_localize_scripts[] = $handle;
+			wp_localize_script( $handle, $name, apply_filters( $name, $data ) );
+		}
+	}
+
+	/**
+	 * Localize scripts only when enqueued.
+	 */
+	public static function localize_printed_scripts() {
+		foreach ( self::$scripts as $handle ) {
+			self::localize_script( $handle );
+		}
+	}
+}
+
+EDD_Reviews_Frontend_Scripts::init();
