cb2d0ce9a34c117ac8167c4481928519ed98cbfd
Mithun Biswas <bhoot.biswas@gmail.com>
2020-06-12 01:37:12 +0600

    Add register_settings() method

--- includes/admin/class-edd-reviews-admin-settings.php
+++ includes/admin/class-edd-reviews-admin-settings.php
@@ -21,7 +21,18 @@ class EDD_Reviews_Admin_Settings {
 	 * Constructor.
 	 */
 	public function __construct() {
+		add_filter( 'edd_settings_sections_extensions', array( $this, 'register_settings_sections' ), 10 );
 		add_filter( 'edd_settings_extensions', array( $this, 'register_settings' ), 10 );
+	}
+
+	/**
+	 * [register_settings_sections description]
+	 * @param  [type] $sections [description]
+	 * @return [type]           [description]
+	 */
+	public function register_settings_sections( $sections ) {
+		$sections['reviews'] = __( 'Reviews', 'easy-digital-downloads' );
+		return $sections;
 	}
 
 	/**
@@ -33,8 +44,8 @@ class EDD_Reviews_Admin_Settings {
 		$settings['reviews'] = array(
 			'enable_reviews' => array(
 				'id'   => 'enable_reviews',
-				'name' => __( 'Allow User Keys', 'easy-digital-downloads' ),
-				'desc' => __( 'Check this box to allow all users to generate API keys. Users with the \'manage_shop_settings\' capability are always allowed to generate keys.', 'easy-digital-downloads' ),
+				'name' => __( 'Enable download reviews', 'easy-digital-downloads' ),
+				'desc' => __( 'Check this box to allow customers to review the products offered on your website.', 'easy-digital-downloads' ),
 				'type' => 'checkbox',
 			),
 		);
